8ef1a608baa0a84fafec5312a98bd2fc9b2e50b3
tolmachevroman <tolmachevroman@gmail.com>
2019-03-12 07:12:01 -0300

    add TwoIntegers Eq and Show tests

--- src/Chapter6/EqInstances.hs
+++ src/Chapter6/EqInstances.hs
@@ -1,7 +1,9 @@
 module Chapter6.EqInstances(
-    TisAnInteger(..)
+    TisAnInteger(..),
+    TwoIntegers(..)
 ) where
 
+-- 1    
 data TisAnInteger = TisAn Integer
 
 instance Show TisAnInteger where
@@ -9,3 +11,12 @@ instance Show TisAnInteger where
 
 instance Eq TisAnInteger where
     (==) (TisAn a) (TisAn a') = a == a'
+
+-- 2
+data TwoIntegers = Two Integer Integer
+
+instance Show TwoIntegers where
+    show (Two a a') = show a ++ " " ++ show a'
+
+instance Eq TwoIntegers where
+    (==) (Two a a') (Two b b') = a == b && a' == b'    
--- test/Chapter6/EqInstancesSpec.hs
+++ test/Chapter6/EqInstancesSpec.hs
@@ -3,5 +3,6 @@ module Chapter6.EqInstancesSpec where
 import SpecHelper    
 
 spec :: Spec
-spec = describe "Instances exercises" $
-        it "TisAnInteger is an instances of Eq and Show" $ TisAn 5 `shouldBe` TisAn 5 
+spec = describe "Instances exercises" $ do
+        it "TisAnInteger is an instance of Eq and Show" $ TisAn 5 `shouldBe` TisAn 5
+        it "TwoIntegers is an instance of Eq and Show" $ Two 5 10 `shouldBe` Two 5 10
