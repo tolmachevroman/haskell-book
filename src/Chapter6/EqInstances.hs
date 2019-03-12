19bd07d34afd1096d44afc2db34807d1de40057e
tolmachevroman <tolmachevroman@gmail.com>
2019-03-12 08:52:19 -0300

    add DayOfWeekFridayTheBest Ord tests

--- src/Chapter6/EqInstances.hs
+++ src/Chapter6/EqInstances.hs
@@ -5,10 +5,11 @@ module Chapter6.EqInstances(
     Pair(..),
     Tuple(..),
     Which(..),
-    EitherOr(..)
+    EitherOr(..),
+    DayOfWeekFridayTheBest(..)
 ) where
 
--- 1    
+-- 1
 data TisAnInteger = TisAn Integer
 
 instance Show TisAnInteger where
@@ -24,15 +25,15 @@ instance Show TwoIntegers where
     show (Two a a') = show a ++ " " ++ show a'
 
 instance Eq TwoIntegers where
-    (==) (Two a a') (Two b b') = a == b && a' == b'    
+    (==) (Two a a') (Two b b') = a == b && a' == b'
 
 -- 3
 data StringOrInt = TisAnInt Int | TisAString String deriving Show
 
 instance Eq StringOrInt where
-    (==) (TisAnInt a) (TisAnInt a') = a == a' 
-    (==) (TisAString b) (TisAString b') = b == b' 
- 
+    (==) (TisAnInt a) (TisAnInt a')     = a == a'
+    (==) (TisAString b) (TisAString b') = b == b'
+
  -- 4
 data Pair a = Pair a a deriving Show
 
@@ -44,7 +45,7 @@ data Tuple a b = Tuple a b deriving Show
 
 instance (Eq a, Eq b) => Eq (Tuple a b) where
     (==) (Tuple a a') (Tuple b b') = a == b && a' == b'
-    
+
 -- 6
 data Which a = ThisOne a | ThatOne a deriving Show
 
@@ -56,5 +57,14 @@ instance Eq a => Eq (Which a) where
 data EitherOr a b = Hello a | Goodbye b deriving Show
 
 instance (Eq a, Eq b) => Eq (EitherOr a b) where
-    (==) (Hello a) (Hello a') = a == a'
+    (==) (Hello a) (Hello a')     = a == a'
     (==) (Goodbye b) (Goodbye b') = b == b'
+
+-- 8
+data DayOfWeekFridayTheBest = Mon | Tue | Wed | Thu | Fri | Sat | Sun deriving (Eq, Show)
+
+instance Ord DayOfWeekFridayTheBest where
+    compare Fri Fri = EQ
+    compare Fri _   = GT
+    compare _ Fri   = LT
+    compare _ _     = EQ
--- test/Chapter6/EqInstancesSpec.hs
+++ test/Chapter6/EqInstancesSpec.hs
@@ -1,23 +1,26 @@
 module Chapter6.EqInstancesSpec where
-    
-import SpecHelper    
+
+import           SpecHelper
 
 spec :: Spec
 spec = describe "Instances exercises" $ do
         it "TisAnInteger is an instance of Eq and Show" $ TisAn 5 `shouldBe` TisAn 5
         it "TwoIntegers is an instance of Eq and Show" $ Two 5 10 `shouldBe` Two 5 10
-        it "StringOrInt is an instance of Eq and Show" $ do 
+        it "StringOrInt is an instance of Eq and Show" $ do
             TisAnInt 3 `shouldBe` TisAnInt 3
             TisAString "bla" `shouldBe` TisAString "bla"
-        it "Pair is an instance of Eq and Show" $ do 
+        it "Pair is an instance of Eq and Show" $ do
             Pair 5 10 `shouldBe` Pair 5 10
             Pair 'c' 'b' `shouldBe` Pair 'c' 'b'
-        it "Tuple is an instance of Eq and Show" $ do 
+        it "Tuple is an instance of Eq and Show" $ do
             Tuple 5 'b' `shouldBe` Tuple 5 'b'
             Tuple "hello" True `shouldBe` Tuple "hello" True
-        it "Which is an instance of Eq and Show" $ do 
+        it "Which is an instance of Eq and Show" $ do
             ThisOne 'b' `shouldBe` ThisOne 'b'
             ThatOne "hello" `shouldBe` ThatOne "hello"
-        it "EitherOr is an instance of Eq and Show" $ do 
+        it "EitherOr is an instance of Eq and Show" $ do
             (Hello 'b' :: EitherOr Char Int) `shouldBe` (Hello 'b' :: EitherOr Char Int)
             (Goodbye "hello" :: EitherOr Int String) `shouldBe` (Goodbye "hello" :: EitherOr Int String)
+        it "DayOfWeekFridayTheBest is an instance of Ord" $ do
+            compare Fri Mon `shouldBe` GT
+            compare Fri Fri `shouldBe` EQ
