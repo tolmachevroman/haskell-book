87c5dc64255cd089591469635e05076b38f3de6c
tolmachevroman <tolmachevroman@gmail.com>
2019-03-16 10:58:19 -0300

    add recursive sum tests

--- src/Chapter8/Recursion.hs
+++ src/Chapter8/Recursion.hs
@@ -1,5 +1,7 @@
 module Chapter8.Recursion(
-    fibonacci
+    fibonacci,
+    dividedBy,
+    recSum
 ) where
 
 -- 1
@@ -7,3 +9,18 @@ fibonacci :: Integral a => a -> a
 fibonacci 0 = 0
 fibonacci 1 = 1
 fibonacci x = fibonacci (x - 1) + fibonacci (x - 2)
+
+-- 2
+type Numerator = Integer
+type Denominator = Integer
+type Quotient = Integer
+
+dividedBy :: Numerator -> Denominator -> Quotient
+dividedBy n d
+    | n < d = 0
+    | otherwise = 1 + dividedBy (n - d) d
+
+-- 3
+recSum :: (Eq a, Num a) => a -> a
+recSum 1 = 1
+recSum n = n + recSum (n - 1)    
--- test/Chapter8/RecursionSpec.hs
+++ test/Chapter8/RecursionSpec.hs
@@ -9,3 +9,11 @@ spec = describe "Recursion exercises" $ do
             fibonacci 5 `shouldBe` 5
             fibonacci 6 `shouldBe` 8
             fibonacci 7 `shouldBe` 13
+        it "dividing in terms of subtraction" $ do
+            dividedBy 10 5 `shouldBe` 2
+            dividedBy 5 10 `shouldBe` 0
+            dividedBy 13 4 `shouldBe` 3
+        it "recursive sum from 1 to n" $ do
+            recSum 5 `shouldBe` 15
+            recSum 6 `shouldBe` 21
+            recSum 2 `shouldBe` 3
