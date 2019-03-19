4bd85d3e2e5d9b717e7f1facfcd567969f6e604e
tolmachevroman <tolmachevroman@gmail.com>
2019-03-19 19:33:03 -0300

    add Ceasar encryption tests

--- test/SpecHelper.hs
+++ test/SpecHelper.hs
@@ -4,11 +4,13 @@ module SpecHelper
   module Chapter6.EqInstances,
   module Chapter7.PatternMatching,
   module Chapter8.Recursion,
-  module Chapter9.Lists
+  module Chapter9.Lists,
+  module Chapter9.Ciphers
 ) where
 
 import           Chapter6.EqInstances
 import           Chapter7.PatternMatching
 import           Chapter8.Recursion
 import           Chapter9.Lists
+import           Chapter9.Ciphers
 import           Test.Hspec
