4beff0e15dc838bbb4151871767887b6f917e58b
tolmachevroman <tolmachevroman@gmail.com>
2019-03-22 08:11:56 -0300

    add filter database numbers test

--- src/Chapter9/Database.hs
+++ src/Chapter9/Database.hs
@@ -1,6 +1,7 @@
 module Chapter9.Database(
     theDatabase,
-    filterDbDate
+    filterDbDate,
+    filterDbNumber
 ) where
 
 import           Data.Time (UTCTime (..), fromGregorian, secondsToDiffTime)
@@ -20,3 +21,9 @@ filterDbDate :: [DatabaseItem] -> [UTCTime]
 filterDbDate = foldr (\i a -> filter' i ++ a ) [] where
     filter' (DbDate t) = [t]
     filter' _          = []
+
+-- 2
+filterDbNumber :: [DatabaseItem] -> [Integer]
+filterDbNumber = foldr (\i a -> filter' i ++ a) [] where
+    filter' (DbNumber t) = [t]
+    filter' _            = []
--- test/Chapter9/DatabaseSpec.hs
+++ test/Chapter9/DatabaseSpec.hs
@@ -10,3 +10,5 @@ spec = describe "Database with folds exercises" $ do
                 UTCTime (fromGregorian 1911 5 1) (secondsToDiffTime 34211),
                 UTCTime (fromGregorian 1921 7 8) (secondsToDiffTime 34211)
                 ]
+        it "filters numbers" $ do
+	    filterDbNumber theDatabase `shouldBe` [9001]
