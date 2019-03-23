ac3c9d2e777500c53d6b179e258a194d21789362
tolmachevroman <tolmachevroman@gmail.com>
2019-03-23 09:21:22 -0300

    add isCar tests

--- test/SpecHelper.hs
+++ test/SpecHelper.hs
@@ -1,20 +1,21 @@
 module SpecHelper
-(
-  module Test.Hspec,
-  module Chapter6.EqInstances,
-  module Chapter7.PatternMatching,
-  module Chapter8.Recursion,
-  module Chapter9.Lists,
-  module Chapter9.Ciphers,
-  module Chapter9.Database,
-  module Chapter10.Folds
-) where
+  ( module Test.Hspec
+  , module Chapter6.EqInstances
+  , module Chapter7.PatternMatching
+  , module Chapter8.Recursion
+  , module Chapter9.Lists
+  , module Chapter9.Ciphers
+  , module Chapter9.Database
+  , module Chapter10.Folds
+  , module Chapter11.Vehicles
+  ) where
 
-import           Chapter6.EqInstances
-import           Chapter7.PatternMatching
-import           Chapter8.Recursion
-import           Chapter9.Lists
-import           Chapter9.Ciphers
-import           Chapter9.Database
-import           Chapter10.Folds
-import           Test.Hspec
+import Chapter10.Folds
+import Chapter11.Vehicles
+import Chapter6.EqInstances
+import Chapter7.PatternMatching
+import Chapter8.Recursion
+import Chapter9.Ciphers
+import Chapter9.Database
+import Chapter9.Lists
+import Test.Hspec
