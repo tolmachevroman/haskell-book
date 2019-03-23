2e69ef610cc4f1bb33b2d589d18d5d249e8dbeab
tolmachevroman <tolmachevroman@gmail.com>
2019-03-22 23:46:15 -0300

    add hindent plug and refactor some files

--- src/Chapter10/Folds.hs
+++ src/Chapter10/Folds.hs
@@ -1,17 +1,18 @@
-module Chapter10.Folds(
-    funcA,
-    funcB,
-    funcC,
-    myOrWithFolds,
-    myAnyWithFolds,
-    myElemWithFolds,
-    myReverseWithFolds,
-    myMapWithFolds,
-    myFilterWithFolds 
-) where
+module Chapter10.Folds
+  ( funcA
+  , funcB
+  , funcC
+  , myOrWithFolds
+  , myAnyWithFolds
+  , myElemWithFolds
+  , myReverseWithFolds
+  , myMapWithFolds
+  , myFilterWithFolds
+  ) where
 
 -- 1
 stops = "pbtdkg"
+
 vowels = "aeiou"
 
 funcA = [(a, b, c) | a <- stops, b <- vowels, c <- stops]
@@ -21,6 +22,7 @@ funcB = [(a, b, c) | a <- stops, b <- vowels, c <- stops, a == 'p']
 
 -- 3
 words' = ["table", "ball", "chair"]
+
 verbs = ["to sit", "to kick", "to wash"]
 
 funcC = [(a, b, c) | a <- words', b <- verbs, c <- words']
@@ -35,7 +37,7 @@ myAnyWithFolds f = foldr (\i a -> f i || a) False
 
 -- 6
 myElemWithFolds :: Eq a => a -> [a] -> Bool
-myElemWithFolds x = foldr (\i a -> i == x || a) False 
+myElemWithFolds x = foldr (\i a -> i == x || a) False
 
 -- 7
 myReverseWithFolds :: [a] -> [a]
@@ -47,4 +49,10 @@ myMapWithFolds f = foldr (\i a -> f i : a) []
 
 -- 9
 myFilterWithFolds :: (a -> Bool) -> [a] -> [a]
-myFilterWithFolds f = foldr (\i a -> if (f i) then i : a else a) [] 
+myFilterWithFolds f =
+  foldr
+    (\i a ->
+       if (f i)
+         then i : a
+         else a)
+    []
--- test/Chapter10/FoldsSpec.hs
+++ test/Chapter10/FoldsSpec.hs
@@ -1,21 +1,22 @@
 module Chapter10.FoldsSpec where
 
-import           SpecHelper
+import SpecHelper
 
 spec :: Spec
-spec = describe "Folds exercises" $ do
-        it "3 tuple of stops and vowels" $ do
-            funcA !! 0 `shouldBe` ('p', 'a', 'p')
-            funcA !! 1 `shouldBe` ('p', 'a', 'b')
-            funcB !! 10 `shouldBe` ('p', 'e', 'k')
-            funcC !! 0 `shouldBe` ("table", "to sit", "table")
-        it "standard functions using folds" $ do
-            myOrWithFolds [False, True] `shouldBe` True
-            myAnyWithFolds (< 0) [1..5] `shouldBe` False
-            myAnyWithFolds even [1..5] `shouldBe` True
-            myElemWithFolds 1 [1..5] `shouldBe` True
-            myElemWithFolds 'a' "Hello" `shouldBe` False
-            myReverseWithFolds [1..3] `shouldBe` [3, 2, 1]
-            myReverseWithFolds "hey" `shouldBe` "yeh"
-            myMapWithFolds even [1..3] `shouldBe` [False, True, False]
-            myFilterWithFolds even [1..10] `shouldBe` [2, 4, 6, 8, 10]
+spec =
+  describe "Folds exercises" $ do
+    it "3 tuple of stops and vowels" $ do
+      funcA !! 0 `shouldBe` ('p', 'a', 'p')
+      funcA !! 1 `shouldBe` ('p', 'a', 'b')
+      funcB !! 10 `shouldBe` ('p', 'e', 'k')
+      funcC !! 0 `shouldBe` ("table", "to sit", "table")
+    it "standard functions using folds" $ do
+      myOrWithFolds [False, True] `shouldBe` True
+      myAnyWithFolds (< 0) [1 .. 5] `shouldBe` False
+      myAnyWithFolds even [1 .. 5] `shouldBe` True
+      myElemWithFolds 1 [1 .. 5] `shouldBe` True
+      myElemWithFolds 'a' "Hello" `shouldBe` False
+      myReverseWithFolds [1 .. 3] `shouldBe` [3, 2, 1]
+      myReverseWithFolds "hey" `shouldBe` "yeh"
+      myMapWithFolds even [1 .. 3] `shouldBe` [False, True, False]
+      myFilterWithFolds even [1 .. 10] `shouldBe` [2, 4, 6, 8, 10]
