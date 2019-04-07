1c399c0fd4a73caa616f40472746d1485d64c773
tolmachevroman <tolmachevroman@gmail.com>
2019-04-07 09:03:59 -0300

    use checkers library for automatic monoid abiding checks

--- src/Chapter15/Monoids.hs
+++ src/Chapter15/Monoids.hs
@@ -1,9 +1,15 @@
 module Chapter15.Monoids where
 
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+
 data Optional a
   = Nada
   | Only a
   deriving (Eq, Show)
+
+instance Eq a => EqProp (Optional a) where
+  (=-=) = eq
 
 instance Semigroup a => Semigroup (Optional a) where
   Nada <> (Only x) = Only x
@@ -13,3 +19,25 @@ instance Semigroup a => Semigroup (Optional a) where
 
 instance Monoid a => Monoid (Optional a) where
   mempty = Nada
+
+newtype First' a = First'
+  { getFirst' :: Optional a
+  } deriving (Eq, Show)
+
+instance Arbitrary a => Arbitrary (Optional a) where
+  arbitrary = oneof [return $ Nada, Only <$> arbitrary]
+
+instance Arbitrary a => Arbitrary (First' a) where
+  arbitrary = First' <$> arbitrary
+
+instance Eq a => EqProp (First' a) where
+  (=-=) = eq
+
+instance Semigroup (First' a) where
+  First' Nada <> First' (Only x) = First' (Only x)
+  First' Nada <> First' Nada = First' Nada
+  First' (Only x) <> First' Nada = First' (Only x)
+  First' (Only x) <> First' (Only y) = First' (Only x)
+
+instance Monoid (First' a) where
+  mempty = First' $ Nada
--- test/Chapter15/MonoidsSpec.hs
+++ test/Chapter15/MonoidsSpec.hs
@@ -1,7 +1,12 @@
+{-# LANGUAGE ViewPatterns #-}
+
 module Chapter15.MonoidsSpec where
 
 import Data.Monoid
 import SpecHelper
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
 
 spec :: Spec
 spec =
@@ -11,3 +16,5 @@ spec =
       Only (Product 4) <> Only (Product 5) `shouldBe` Only (Product 20)
       Only (Sum 1) <> Nada `shouldBe` Only (Sum 1)
       Only [1] <> Nada `shouldBe` Only [1]
+    it "Monoid instance of First'" $ do
+      quickBatch (monoid $ First' (Only 0 :: Optional Int))
